0e0e751a2a89e9d9eb711fe7ed8722955328396c
Aryaveer Sharma <24csu306@ncuindia.edu>
2025-09-19 00:07:22 +0530

    Create FahrenheitToCelsius.java

---
+++ FahrenheitToCelsius.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class FahrenheitToCelsius {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter temperature in Fahrenheit: ");
+        double f = sc.nextDouble();
+        
+        double c = (f - 32) * 5 / 9;
+        System.out.println("Celsius: " + c);
+
+        System.out.println("Aryaveer Sharma 24csu306");
+    }
+}
